219007f1eda2c9e15c9359088aba6015370f5ef7
yuzhi233 <328153843@qq.com>
2020-02-06 21:43:20 +0800

    安装成功测试代码！

---
+++ 测试是否安装成功.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 21:33:29 2020
+
+@author: zhoubo
+"""
+
+
+import torch
+ 
+print(torch.__version__)
+print(torch.cuda.is_available())
+#终于安装成功了！！！
+#CUDA安装成功了！！耶耶耶好麻烦啊
